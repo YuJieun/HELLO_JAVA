24a0a9099d6dd0aa6db4a62a9d0d9304c3075e2f
YuJieun <idpresent@naver.com>
2019-05-26 14:57:49 +0900

    test 2

---
+++ Test0526/src/Q_2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_2 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = br.readLine();
+		String[] arr = tmp.split(" ");
+		
+		int max = -1;
+		for(int i=0;i<arr.length;i++) {
+			if(Integer.parseInt(arr[i])>max)
+				max = Integer.parseInt(arr[i]);
+		}
+		
+		int[] count = new int[max+1];
+		
+		for(int i=0; i<arr.length;i++) {
+			count[Integer.parseInt(arr[i])]++;
+		}
+		
+		boolean flag = true;
+		for(int i=1;i<=count.length;i++) {
+			if(count[i]!=1) {
+				System.out.println("false");
+				flag = false;
+				break;
+			}
+		}
+		
+		if(flag == true)
+			System.out.println("true");
+	}
+
+}
