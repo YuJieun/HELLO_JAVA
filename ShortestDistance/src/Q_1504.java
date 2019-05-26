5ff43e77b2112cf0f04ea49fabdd643e0f4849c9
YuJieun <idpresent@naver.com>
2019-05-26 14:14:00 +0900

    1504 new version

--- ShortestDistance/src/Q_1504.java
+++ ShortestDistance/src/Q_1504.java
@@ -3,90 +3,83 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Q_1504 {
-	static int[][] dis;
-	static ArrayList<Point3>[] arr;
-	static int n,e;
-	//여기서 INF를 Integer.MAX_VALUE로 하면 틀리고 800*1000을 하면 맞음.
-	//이유를 모르겠음
-	static final int INF = 800*1000;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		
-		arr = new ArrayList[n+1];
-		//다익스트라 세번호출
-		dis = new int[3][n+1];
-		
-		for(int i=0;i<3;i++) {
-			Arrays.fill(dis[i], INF);
-		}
-		
-		for(int i=1;i<=n;i++) {
-			arr[i] = new ArrayList<>();
-		}
-			
-		for(int i=0;i<e;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			arr[a].add(new Point3(b,c));
-			arr[b].add(new Point3(a,c));
-		}
-		st = new StringTokenizer(br.readLine());
-		int first = Integer.parseInt(st.nextToken());
-		int second = Integer.parseInt(st.nextToken());
-		
-		//시작경로, 몇번째 다익스트라호출인지
-		dijkstra(1,0);
-		dijkstra(first,1);
-		dijkstra(second,2);
-		
-		int minCost = Math.min(dis[0][first]+dis[1][second]+dis[2][n], dis[0][second]+dis[2][first]+dis[1][n]);
-		System.out.println(minCost>=INF?-1:minCost);
-	}
-	
-	public static void dijkstra(int src, int idx) {
-		PriorityQueue<Point3> pq = new PriorityQueue<Point3>();
-		
-		pq.add(new Point3(src,0));
-		dis[idx][src] = 0;
-		
-		while(!pq.isEmpty()) {
-			Point3 tmp = pq.poll();
-			
-			if(dis[idx][tmp.to]<tmp.cost)
-				continue;
-			
-			for(int j=0; j<arr[tmp.to].size(); j++) {
-				Point3 next = arr[tmp.to].get(j);
-				if(dis[idx][next.to] > dis[idx][tmp.to]+next.cost) {
-					dis[idx][next.to] = dis[idx][tmp.to]+next.cost;
-					pq.add(new Point3(next.to,dis[idx][next.to]));
-				}
-			}
-		}
-	}
+    static ArrayList<Point3>[] arrayLists;
+    static int N;
+    static int INF = 100000000;
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] NE = bufferedReader.readLine().split(" ");
+        N = Integer.parseInt(NE[0]);
+        int E = Integer.parseInt(NE[1]);
+
+        arrayLists = new ArrayList[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            arrayLists[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            String[] input = bufferedReader.readLine().split(" ");
+            int point1 = Integer.parseInt(input[0]);
+            int point2 = Integer.parseInt(input[1]);
+            int cost = Integer.parseInt(input[2]);
+            arrayLists[point1].add(new Point3(point2, cost));
+            arrayLists[point2].add(new Point3(point1, cost));
+        }
+        String[] must = bufferedReader.readLine().split(" ");
+        int must1 = Integer.parseInt(must[0]);
+        int must2 = Integer.parseInt(must[1]);
+
+        int[] distance1 = new int[N + 1]; //1에서 must1,must2
+        int[] distance2 = new int[N + 1]; //must1에서 must2
+        int[] distance3 = new int[N + 1]; //n에서 must1,must2
+        distance1 = dijkstra(1);
+        distance2 = dijkstra(must1);
+        distance3 = dijkstra(N);
+
+        int min = Math.min(distance1[must1] + distance2[must2] + distance3[must2], distance1[must2] + distance2[must2] + distance3[must1]);
+        System.out.println(min >= INF ? -1 : min);
+    }
+
+    public static int[] dijkstra(int start) {
+        int[] distance = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        Arrays.fill(distance, INF);
+        distance[start] = 0;
+
+        PriorityQueue<Point3> queue = new PriorityQueue<>();
+        queue.offer(new Point3(start, 0));
+        while (!queue.isEmpty()) {
+            Point3 point = queue.poll();
+            if (visited[point.dest] == true) {
+                continue;
+            }
+            visited[point.dest] = true;
+
+            for (Point3 next : arrayLists[point.dest]) {
+                if (visited[next.dest] == false) {
+                    distance[next.dest] = Math.min(distance[next.dest], distance[point.dest] + next.cost);
+                    queue.offer(new Point3(next.dest, distance[next.dest]));
+                }
+            }
+        }
+        return distance;
+    }
 }
 
-class Point3 implements Comparable<Point3>{
-	int to, cost;
-	
-	Point3(int to, int cost){
-		this.to = to;
-		this.cost = cost;
-	}
-	
-	@Override
-	public int compareTo(Point3 p) {
-		return this.cost - p.cost;
-	}
+class Point3 implements Comparable<Point3> {
+    int dest;
+    int cost;
+
+    Point3(int dest, int cost) {
+        this.dest = dest;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Point3 o) {
+        return this.cost > o.cost ? 1 : -1;
+    }
 }
