1cccbc18c6a19959d9daaec2ce9518eb0c9081b3
YuJieun <idpresent@naver.com>
2019-09-08 18:28:53 +0900

    쇠막대기

---
+++ Programmers/src/StackQueue_Sticks_Dequeue.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String arrangement) {
+        int answer = 0;
+        Deque<Character> dq = new LinkedList<>();
+
+        
+        for(int i=0; i<arrangement.length();i++){
+            dq.addLast(arrangement.charAt(i));
+        }
+        
+        int stick = 0;
+        while(!dq.isEmpty()){
+            if(dq.removeFirst()=='('){
+                if(dq.peekFirst()==')'){
+                    dq.removeFirst();
+                    answer += stick;
+                }else{
+                    stick++;
+                }
+            }
+            else{
+                answer += 1;
+                stick--;
+            }
+        }
+        return answer;
+    }
+}
