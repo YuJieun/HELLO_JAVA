40ec11622cb4580f98eef78c62d675f1812d14ec
YuJieun <idpresent@naver.com>
2019-10-08 12:54:49 +0900

    전화번호부

---
+++ programmers_hash/src/programmers_hash/Q_2.java
@@ -0,0 +1,21 @@
+package programmers_hash;
+
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String,Integer> map = new HashMap();
+        Arrays.sort(phone_book);
+
+        for(int i=0; i<phone_book.length-1; i++){
+            for(int j=i+1; j<phone_book.length; j++){
+                if(phone_book[j].startsWith(phone_book[i])){
+                    answer = false;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
