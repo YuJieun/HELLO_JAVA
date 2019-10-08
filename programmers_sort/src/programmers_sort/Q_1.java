c9f65c3e5c9c40f365d3c08ffaf01c1b5ad26b65
YuJieun <idpresent@naver.com>
2019-10-08 14:57:12 +0900

    k번째수

---
+++ programmers_sort/src/programmers_sort/Q_1.java
@@ -0,0 +1,19 @@
+package programmers_sort;
+
+import java.util.*;
+
+class Q_1 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++){
+            int[] tmp = new int[commands[i][1]-commands[i][0]+1];
+            for(int j=0; j<tmp.length; j++){
+                tmp[j] = array[j+commands[i][0]-1];
+            }
+            Arrays.sort(tmp);
+            answer[i] = tmp[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
