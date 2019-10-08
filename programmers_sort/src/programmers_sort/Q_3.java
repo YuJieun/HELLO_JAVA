499eb0348b92b88c447b6afb90c34721533025b8
YuJieun <idpresent@naver.com>
2019-10-08 15:57:22 +0900

    정렬

---
+++ programmers_sort/src/programmers_sort/Q_3.java
@@ -0,0 +1,19 @@
+package programmers_sort;
+
+import java.util.*;
+
+class Q_3 {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+        
+        for(int i = 0; i < citations.length; i++) {
+            if (citations[i]>= citations.length-i) {
+                answer = citations.length-i;
+                break;
+            }
+        }  
+        return answer;   
+    }
+}
