5fdfce3ebd2cd77bc696a6f8fe40996837ab8a17
YuJieun <idpresent@naver.com>
2019-10-10 17:03:47 +0900

    여행경로 수정

--- programmers_DFS_BFS/src/programmers_DFS_BFS/Q_4.java
+++ programmers_DFS_BFS/src/programmers_DFS_BFS/Q_4.java
@@ -3,44 +3,38 @@ package programmers_DFS_BFS;
 import java.util.*;
 
 class Q_4 {
+    boolean[] visited;
+    String tmp="";
+    ArrayList<String> arr = new ArrayList<>();
+    
     public String[] solution(String[][] tickets) {
-        String[] answer = new String[tickets.length+1];
-        boolean[] use = new boolean[tickets.length];
-        PriorityQueue<Object> q = new PriorityQueue<Object>();
+        String[] answer ={};
         for(int i=0; i<tickets.length; i++){
-            if(tickets[i][0].equals("ICN"))
-                q.offer(new Object(tickets[i][1],i));
+            if(tickets[i][0].equals("ICN")){
+                visited = new boolean[tickets.length];
+                tmp = "ICN";
+                visited[i] = true;
+                dfs(tickets[i][1],tickets, 1);
+            }
         }
-        answer[0] = "ICN";
-        int cnt=1;
-        
-        while(!q.isEmpty()){
-            Object airport = q.poll();
-            q.clear();
-            System.out.print(airport.des);
-            answer[cnt++] = airport.des;
-            use[airport.ticket_num] = true;
-            for(int i=0; i<tickets.length; i++){
-                if(use[i]==false && tickets[i][0].equals(airport.des)){
-                    q.offer(new Object(tickets[i][1],i));
-                }
-            }     
-        }    
+        Collections.sort(arr);
+        answer = arr.get(0).split(",");
         return answer;
     }
-}
-
-class Object implements Comparable<Object> {
-    String des;
-    int ticket_num;
     
-    public Object(String d, int t){
-        this.des = d;
-        this.ticket_num = t;
-    }
-    
-    @Override
-    public int compareTo(Object target) {
-        return this.des.compareTo(target.des);
+    public void dfs(String airport, String[][] tickets, int cnt){
+        tmp = tmp +","+airport+"";
+        if(cnt==tickets.length){
+            arr.add(tmp);
+            return;
+        }
+        for(int j=0; j<tickets.length; j++){
+            if(visited[j]==false && airport.equals(tickets[j][0])){
+                visited[j] = true;
+                dfs(tickets[j][1],tickets,cnt+1);
+                visited[j] = false;
+                tmp = tmp.substring(0, tmp.length()-4);
+            }
+        }
     }
 }
