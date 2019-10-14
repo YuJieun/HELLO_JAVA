530c7b03e10adccbfb7825b14701e927cd5af584
YuJieun <idpresent@naver.com>
2019-10-14 23:25:34 +0900

    사다리 조작

--- SW_AGAIN/src/Q_15684.java
+++ SW_AGAIN/src/Q_15684.java
@@ -1,67 +1,65 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Q_15684 {
-	static int min = Integer.MAX_VALUE;
-	static int line,n,m;
-	static int[][] map;
-	static boolean[][] visited;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		m = Integer.parseInt(st.nextToken());
-		line = Integer.parseInt(st.nextToken());
-		n = Integer.parseInt(st.nextToken());
-		
-		map = new int[m+2][n+2];
-		for(int i=0; i<line; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			map[a][b] = 1;
-		}	
-		dfs(1,1,0);
-		System.out.println(min==4?-1:min);
-        	}
-	
-	public static void dfs(int x, int y, int cnt) {
-		if(y==m+1) {
-			x +=1;
-			y=1;
-		}
-		if(cnt == 3 || (x==n&&y==m)) {
-			if(check())
-				min = Math.min(min,cnt);
-				return;
-		}
-		
-		dfs(x,y+1,cnt);
-		if(y==m || map[x][y]==1 || (y!=1 && map[x][y-1]==1 || map[x][y+1] ==1))
-			return;
-		
-		map[x][y]=1;
-		dfs(x,y+1,cnt+1);
-		map[x][y] = 0;
-	}
-	
-	public static boolean check() {
-		for(int i=1; i<=m; i++) {
+	static int n,m,h,ans=4;
+    static boolean[][] a;
+    static boolean check(boolean[][] a) {
+        for(int i=1; i<=m; i++) {
             int start = i, end = i;
             for(int j=1; j<=n; j++) {
-                if(map[j][end]==1) {
+                if(a[j][end]) {
                     ++end;
                     continue;
                 }
                 if(end==1) continue;
-                if(map[j][end]==0) {
-                    if(map[j][end-1]==1) --end;
+                if(!a[j][end]) {
+                    if(a[j][end-1]) --end;
                 }
             }
             if(start!=end) return false;
         }
         return true;
-	}
+    }
+    static void go(int x, int y, int cnt, boolean[][] a) {
+        if(y==m+1) {
+            ++x;
+            y=1;
+        }
+        if(cnt==3 || (x==n && y==m)) {
+            if(check(a))
+                ans = Math.min(ans, cnt);
+            return;
+        }
+         
+        //설치 안하거나
+        go(x, y+1, cnt, a);
+         
+        //인접한 가로선이 있을 때
+        if(y==m || a[x][y] || (y!=1 && a[x][y-1]) || a[x][y+1]) return;
+         
+        //설치하거나
+        a[x][y] = true;
+        go(x, y+1, cnt+1, a);
+        a[x][y] = false;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        m = in.nextInt();
+        h = in.nextInt();
+        n = in.nextInt();
+        a = new boolean[n+2][m+2];
+         
+        for(int i=0; i<h; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            a[x][y] = true;
+        }
+         
+        go(1,1,0,a);
+        System.out.println(ans==4?-1:ans);
+        in.close();
+    }
 }
